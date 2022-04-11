cee924d9698a2f9b4d95c3b0e4b768e65f411092
Kenji <97561440+sanehirakenji@users.noreply.github.com>
2022-04-11 10:16:49 -1000

    Added User Component

---
+++ app/imports/ui/components/User.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+class User extends React.Component {
+  render() {
+    return (
+      <Card>
+        <Card.Content>
+          <Image
+            floated='right'
+            size='mini'
+            src={this.props.user.profilePicture}
+          />
+          <Card.Header>{this.props.user.firstName} {this.props.user.lastName}</Card.Header>
+          <Card.Meta>Arrival: {this.props.user.arriveTime}, Leave: {this.props.user.leaveTime}</Card.Meta>
+          <Card.Description>{this.props.user.bio}</Card.Description>
+        </Card.Content>
+        <Card.Content>
+          {this.props.user.contact}
+        </Card.Content>
+      </Card>
+    );
+  }
+}
+
+User.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default withRouter(User);
