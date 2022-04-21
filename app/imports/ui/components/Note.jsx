ee23b2c71249a13f8aacb6abe9143fc2e9adf18d
kaialii <31085175+kaialii@users.noreply.github.com>
2022-04-21 12:38:25 -1000

    working on it... ;(

--- app/imports/ui/components/Note.jsx
+++ app/imports/ui/components/Note.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Feed } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Notes } from '../../api/note/Notes';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Note extends React.Component {
   render() {
+    const note = this.props.note;
     return (
       <Feed.Event >
         <Feed.Content>
-          <Feed.Date content={this.props.note.createdAt.toLocaleDateString('en-US')} />
           <Feed.Summary>
-            {this.props.note.note}
+            {note.firstName}
           </Feed.Summary>
         </Feed.Content>
       </Feed.Event>
@@ -19,10 +21,23 @@ class Note extends React.Component {
   }
 }
 
-// Require a document to be passed to this component.
+/** Require an array of Stuff documents in the props. */
 Note.propTypes = {
-  note: PropTypes.object.isRequired,
+  note: PropTypes.array.isRequired,
+  doc: PropTypes.object,
+  ready: PropTypes.bool.isRequired,
 };
 
-// Wrap this component in withRouter since we use the <Link> React Router element.
-export default withRouter(Note);
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(({ match }) => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const documentId = match.params._id;
+  // Ensure that minimongo is populated with all collections prior to running render().
+  const sub1 = Meteor.subscribe(Notes.userPublicationName);
+  // Get the document
+  const doc = Notes.collection.findOne(documentId);
+  return {
+    doc,
+    ready: sub1.ready(),
+  };
+})(Note);
