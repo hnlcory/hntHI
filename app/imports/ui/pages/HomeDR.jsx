4ddecf8967f9b31d115ccc7faf8fb3faae7319e2
Cory-Mac <school.coryparker@gmail.com>
2022-04-28 10:54:01 -1000

    ui fixes, add map

--- app/imports/ui/pages/HomeDR.jsx
+++ app/imports/ui/pages/HomeDR.jsx
@@ -38,6 +38,9 @@ class HomeDR extends React.Component {
             </Grid.Column>
           </Grid>
         </div>
+        <iframe
+          /* eslint-disable-next-line max-len */
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3717.356025878127!2d-157.819300485067!3d21.296943983944583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c006d989580d855%3A0xac63f2de838ed2f4!2sUniversity%20of%20Hawai%CA%BBi%20at%20M%C4%81no!5e0!3m2!1sen!2sus!4v1649541864645!5m2!1sen!2sus' style={{ overflow: 'hidden', width: '100%', height: '300px', border: '0', allowFullScreen: '', loading: 'lazy', referrerPolicy: 'no-referrer-when-downgrade' }}/>
         <div className='homedr-picture-background'>
           <Container textAlign='center'>
             <Header style={{ color: 'white', fontSize: '50pt', paddingTop: '50px', fontFamily: 'Barlow' }} as='h1'>
