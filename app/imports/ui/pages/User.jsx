2c343963f176fc6d15cfe329dbb0aa915336926f
Cory-Mac <school.coryparker@gmail.com>
2022-04-28 11:17:51 -1000

    removed old link

--- app/imports/ui/pages/User.jsx
+++ app/imports/ui/pages/User.jsx
@@ -52,7 +52,6 @@ const MakeCard = (props) => (
         <Header as="h4"> Arrives: {props.profile.arriveTime} | Leaves {props.profile.leaveTime}</Header>
         <Header as="h4"> Contact me: {props.profile.contact}</Header>
         <Header as="h4">Star Rating: {props.profile.rating} <Icon name='star'/></Header>
-        <Link id='edit-button' color='blue' to={`/useredit/${props.profile._id}`}>Edit my profile</Link>
         <Button basic color='blue' size='tiny' as={Link} to={`/useredit/${props.profile._id}`}><Icon name='edit outline'/>
           Edit my profile</Button>
       </Grid.Column>
