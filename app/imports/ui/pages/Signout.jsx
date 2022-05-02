da035ed69aa006db3ba56c61998948306abc6e07
Cory-Mac <school.coryparker@gmail.com>
2022-05-01 19:16:04 -1000

    fixes to signout, landing page

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Image } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
   render() {
     Meteor.logout();
     return (
-      <Container>
-        <Header id="signout-page" as="h2" textAlign="center">
-          <p>You have signed out successfully.</p>
-        </Header>
+      <div className='landing-picture-background' style={{ paddingTop: '30px', paddingBottom: '20px' }}>
         <Image src='/images/logov1.jpeg' size='small' circular centered/>
-      </Container>
+        <Header id="signout-page" as="h2" textAlign="center">
+          <p style={{ color: 'white', fontSize: '20pt', paddingTop: '30px', fontFamily: 'Barlow' }}>You have signed out successfully.</p>
+        </Header>
+      </div>
     );
   }
 }
