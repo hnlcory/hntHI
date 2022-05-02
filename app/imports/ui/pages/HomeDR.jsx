62e4fd6859d1ff1300812e9fb921313bc27db3b2
Cory Parker <school.coryparker@gmail.com>
2022-05-01 19:16:29 -1000

    Issue 72 (#101)
    * white text fix on dark mode
    
    * padding fixes
    
    * font corrections
    
    * change order of homeDR pages
    
    * fixes to signout, landing page

--- app/imports/ui/pages/HomeDR.jsx
+++ app/imports/ui/pages/HomeDR.jsx
@@ -49,19 +49,19 @@ class HomeDR extends React.Component {
             </Grid.Column>
           </Grid>
         </div>
-        <iframe
-          /* eslint-disable-next-line max-len */
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3717.356025878127!2d-157.819300485067!3d21.296943983944583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c006d989580d855%3A0xac63f2de838ed2f4!2sUniversity%20of%20Hawai%CA%BBi%20at%20M%C4%81no!5e0!3m2!1sen!2sus!4v1649541864645!5m2!1sen!2sus' style={{ overflow: 'hidden', width: '100%', height: '300px', border: '0', allowFullScreen: '', loading: 'lazy', referrerPolicy: 'no-referrer-when-downgrade' }}/>
-        <div className='landing-picture-background'>
+        <div className='landing-person-background'>
           <Container textAlign='center'>
-            <Image src='/images/logov1.jpeg' size='small' centered circular style={{ paddingTop: '10px' }}/>
-            <Header style={{ color: 'white', fontSize: '50pt', paddingTop: '10px', fontFamily: 'Barlow' }} as='h1'>
-              Our Contribution To You
+            <Header style={{ color: 'white', fontSize: '50pt', paddingTop: '50px', fontFamily: 'Barlow' }} as='h1'>
+              The Fast Ride Way
             </Header>
-            <Header style={{ paddingTop: '20px', paddingBottom: '70px', color: 'white', fontSize: '25t', fontFamily: 'Barlow' }} as='h3'>
-              We aim to give University of Hawaii at Manoa students to feel that there are more options than having to pay
-              endless amounts of commuting fees such as gas or parking. Our aina is meant to be shared and cared for, so let us
-              do our parts to work together to improve your daily commutes
+            <Header style={{ paddingTop: '50px', paddingBottom: '70px', color: 'white', fontSize: '28t', fontFamily: 'Barlow' }} as='h3'>
+              Need a ride on short notice? Got some time and can offer someone a ride? The Fast Ride is a form feed allows
+              both riders and drivers to see a real-time chat for those needing to get somewhere soon. <br/><br/>
+              <Button.Group size='large'>
+                <Button id='feed-button' color='olive' as={Link} to='/fastridefeed'>Check the Feed</Button>
+                <Button.Or />
+                <Button id='form-button' as={Link} to='/fastrideform'>Create a Request Form</Button>
+              </Button.Group>
             </Header>
           </Container>
         </div>
@@ -86,20 +86,20 @@ class HomeDR extends React.Component {
               <Image src="/images/signup.png"/>
             </Grid.Column>
           </Grid>
+          <iframe
+            /* eslint-disable-next-line max-len */
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3717.356025878127!2d-157.819300485067!3d21.296943983944583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c006d989580d855%3A0xac63f2de838ed2f4!2sUniversity%20of%20Hawai%CA%BBi%20at%20M%C4%81no!5e0!3m2!1sen!2sus!4v1649541864645!5m2!1sen!2sus' style={{ overflow: 'hidden', width: '100%', height: '300px', border: '0', allowFullScreen: '', loading: 'lazy', referrerPolicy: 'no-referrer-when-downgrade' }}/>
         </div>
-        <div className='landing-person-background'>
+        <div className='landing-picture-background'>
           <Container textAlign='center'>
-            <Header style={{ color: 'white', fontSize: '50pt', paddingTop: '50px', fontFamily: 'Barlow' }} as='h1'>
-              The Fast Ride Way
+            <Image src='/images/logov1.jpeg' size='small' centered circular style={{ paddingTop: '10px' }}/>
+            <Header style={{ color: 'white', fontSize: '50pt', paddingTop: '10px', fontFamily: 'Barlow' }} as='h1'>
+              Our Contribution To You
             </Header>
-            <Header style={{ paddingTop: '50px', paddingBottom: '70px', color: 'white', fontSize: '28t', fontFamily: 'Barlow' }} as='h3'>
-              Need a ride on short notice? Got some time and can offer someone a ride? The Fast Ride is a form feed allows
-              both riders and drivers to see a real-time chat for those needing to get somewhere soon. <br/><br/>
-              <Button.Group size='large'>
-                <Button id='feed-button' color='olive' as={Link} to='/fastridefeed'>Check the Feed</Button>
-                <Button.Or />
-                <Button id='form-button' as={Link} to='/fastrideform'>Create a Request Form</Button>
-              </Button.Group>
+            <Header style={{ paddingTop: '20px', paddingBottom: '70px', color: 'white', fontSize: '25t', fontFamily: 'Barlow' }} as='h3'>
+              We aim to give University of Hawaii at Manoa students to feel that there are more options than having to pay
+              endless amounts of commuting fees such as gas or parking. Our aina is meant to be shared and cared for, so let us
+              do our parts to work together to improve your daily commutes
             </Header>
           </Container>
         </div>
