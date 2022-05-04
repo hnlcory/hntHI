1e89bdd4bba0c0642c6aabec45531a37fffd77a1
Cory-Mac <school.coryparker@gmail.com>
2022-05-03 16:52:11 -1000

    prompt now executes delete function but the delete function needs the proper id passed, not sure how that is going to be passed in

--- app/imports/ui/pages/ConfirmPrompt.jsx
+++ app/imports/ui/pages/ConfirmPrompt.jsx
@@ -6,27 +6,32 @@ import { Link } from 'react-router-dom';
 import { Users } from '../../api/users/Users';
 import { UsersLocations } from '../../api/users/UsersLocations';
 
-function deleteCard(usrID) {
-  // find email from id in users collection
-  const usrEmail = _.pluck(Users.collection.find({ _id: usrID }).fetch(), 'email');
-  // remove from user
-  Users.collection.remove({ _id: usrID });
-  // find location id with email
-  // remove from location
-  const usrLocID = _.pluck(UsersLocations.collection.find({ profile: usrEmail[0] }).fetch(), '_id');
-  UsersLocations.collection.remove({ _id: usrLocID[0] });
-  swal('Success', 'Account Deleted Successfully', 'success');
-  // setup to possibly delete the default account signing data
-  // const acc = _.pluck(Users.collection.find({ username: usrEmail[0] }).fetch(), '_id');
-  // console.log(acc);
-}
-
 class ConfirmPrompt extends Component {
+
+  deleteCard(usrID) {
+    this.setState({ open: false });
+    // find email from id in users collection
+    const usrEmail = _.pluck(Users.collection.find({ _id: usrID }).fetch(), 'email');
+    console.log(usrEmail);
+    // remove the added fields from user so the user is prompted to make another profile.
+    Users.collection.update({ _id: usrID }, { $unset: { firstName: 1, lastName: 1,
+      role: 1, profilePicture: 1, bio: 1, arriveTime: 1,
+      leaveTime: 1, location: 1, contact: 1, rating: 1 } }, false, true);
+    // find location id with email
+    // remove from location
+    const usrLocID = _.pluck(UsersLocations.collection.find({ profile: usrEmail[0] }).fetch(), '_id');
+    UsersLocations.collection.remove({ _id: usrLocID[0] });
+    swal('Success', 'Account Deleted Successfully', 'success');
+    // setup to possibly delete the default account signing data
+    // const acc = _.pluck(Users.collection.find({ username: usrEmail[0] }).fetch(), '_id');
+    // console.log(acc);
+  }
+
   state = { open: false }
 
   show = () => this.setState({ open: true })
 
-  handleConfirm = () => this.setState({ open: false })
+  handleConfirm = () => this.deleteCard(); // needs some way to get the profile id
 
   handleCancel = () => this.setState({ open: false })
 
